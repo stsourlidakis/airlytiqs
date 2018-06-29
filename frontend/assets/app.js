10e72cd05508b438b94efc0e7e07e9c33bba1fbc
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2018-06-29 17:41:11 +0300

    add OpenLayers

--- frontend/assets/app.js
+++ frontend/assets/app.js
@@ -1,1 +1,12 @@
-console.log('Its alive!');
+const osmLayer = new ol.layer.Tile({
+	source: new ol.source.OSM()
+});
+
+const map = new ol.Map({
+	layers: [osmLayer],
+	target: "mapWrapper",
+	view: new ol.View({
+		center: ol.proj.transform([ 23.69318, 37.94187 ], 'EPSG:4326', 'EPSG:3857'),
+		zoom: 16
+	})
+});
