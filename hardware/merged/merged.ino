9607c4d62d077af95cab918ba2479d71c0a28530
DBm <dbarmpakos@outlook.com>
2018-06-30 13:12:40 +0300

    add pack and send overloading, dust measure function

--- hardware/merged/merged.ino
+++ hardware/merged/merged.ino
@@ -1,5 +1,5 @@
 #include "gsm_fun.h" // gprs_functions
-float Vis, IR, UV;
+float Vis, IR, UV, UVindex;
 
 const int samplingTime = 280;     //** follows http://www.howmuchsnow.com/arduino/airquality/
 const int sleepTime = 9680;       //**
@@ -10,70 +10,63 @@ float calcVoltage = 0;
 float dustDensity = 0;
 
 
-char packet[64] = 	{'\0'};
-char buffer [25] = 	{'\0'};
+char packet[64] =   {'\0'};
+char buffer [25] =  {'\0'};
+int CORaw;
+int COppm;
+bool hotState, readyToMeas;
+unsigned long previousMillis = 0;
+long OnTime = 5000;           // milliseconds of on-time
+long OffTime = 1000;          // milliseconds of off-time
+
+
+
 
 void setup () {
 
-  Serial.begin(9600);  	// Init UART 96008N1
+  Serial.begin(9600);   // Init UART 96008N1
   pinMode(ledPower, OUTPUT);
+  pinMode(heater, OUTPUT);
+
   uv.begin();
-  modem.begin(9600);		// init NeoSWSerial 96008N1
+  modem.begin(9600);    // init NeoSWSerial 96008N1
 
 }
 
 void loop() {
+
+  unsigned long currentMillis = millis(); // timekeeping
+ 
   /* measure first */
   /* measure UV */
-  Vis = uv.readVisible();
-  IR = uv.readIR();
-  UV = uv.readUV();
-
-  Serial.println(Vis);
-  Serial.println(IR);
-  Serial.println(UV);
-
-
+  float Vis = uv.readVisible();
+  float IR = uv.readIR();
+  float UV = uv.readUV();
+  float UVindex = UV/100.0;
   /* UV ********* */
-  /* measure dust */
-  digitalWrite(ledPower, LOW);           			// LED ON
-  delayMicroseconds(samplingTime);
-  voMeasured = analogRead(measurePin);  			// ADC value
-  delayMicroseconds(deltaTime);
-  digitalWrite(ledPower, HIGH);          			// LED ON
-  delayMicroseconds(sleepTime);
-  calcVoltage = voMeasured * (5.0 / 1024);
-  dustDensity = (0.17 * calcVoltage - 0.1) * 1000; // Chris Nafis (c) 2012
-  dustDensity += 70;
-  /* dust ******* */
+  /* dust */
+  dustDensity = dust_measure(samplingTime, deltaTime, sleepTime);
+  /* dust */
 
-  Serial.println(dustDensity); 
-  
-  itoa (Vis, buffer, 10);
+  /* then pack */ 
+  itoa (UV, buffer, 10);
   strcpy(packet, buffer);
   buffer[0] = (char)0;
+  pack_and_flush(IR, buffer, packet);
+  pack_and_flush(Vis, buffer, packet);
+  pack_and_flush(UVindex, buffer, packet);
+  /* then pack */ 
 
-  itoa (IR, buffer, 10);
-  strcat (packet, buffer);
-  buffer[0] = (char)0;
 
-  itoa (UV, buffer, 10);
-  strcat (packet, buffer);
-  buffer[0] = (char)0;
-
-  itoa (dustDensity, buffer, 10);
-  strcat (packet, buffer);
-  buffer[0] = (char)0;
-  for (byte i = 0; i < 255; i++) Serial.print(packet[i]);
-  Serial.println();
+  /* then send */ 
   while (!ada_fona.begin(modem)) delay(1000); // give time to provider to register us
 
-  byte notRegistered;							// counter to handle registration iterations
-  while (get_net_stat() != 1)					// netstat = 1 || 5, depends on SIM provider
+  byte notRegistered;             // counter to handle registration iterations
+  while (get_net_stat() != 1)         // netstat = 1 || 5, depends on SIM provider
   {
     delay(5000);
-    notRegistered++;							// increase registration iteration count
-    if (notRegistered == 127) 				// arbitary
+    notRegistered++;              // increase registration iteration count
+    if (notRegistered == 127)         // arbitary
     {
       Serial.println(F("get_net_stat() failed"));
       notRegistered = 0;
@@ -85,14 +78,14 @@ void loop() {
   // ******************************************************************************************************************************************************
   ada_fona.setGPRSNetworkSettings(F("internet"),
                                   F(""),
-                                  F("")); 	// setup APN
-  byte upFail, postFail;						// counters to handle GPRS lock and POST
+                                  F(""));   // setup APN
+  byte upFail, postFail;            // counters to handle GPRS lock and POST
   bool passedGPRS = false;
   while (!ada_fona.enableGPRS(true))
   {
-    upFail++;									// increase GPRS lock iteration count
+    upFail++;                 // increase GPRS lock iteration count
     delay(1000);
-    if (upFail == 127)						// arbitary
+    if (upFail == 127)            // arbitary
     {
       Serial.println(F("GPRS lock failed"));
       upFail = 0;
@@ -100,6 +93,7 @@ void loop() {
     }
   } passedGPRS = true;
 
+  char packet[] = "123"; // placeholder for sensor data
   for (byte i = 0; i < 254; i++) http_post("http://airlytiqs.cloudapp.net/telemetry", packet); // super scary, will change
   flush_FONA(); // flash NeoSWSerial
   packet[0] = (char)0;
